fb64bd4859322b8daafde4957899f49fcc6a61a8
Marcelo Bentivoglio <mabj90s@gmail.com>
2020-11-20 17:46:52 -0300

    added gif-list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
 
 class App extends Component {
 
 
 	render() {
+		const gifs =[
+			{ id: "xT9IgDEI1iZyb2wqo8" },
+			{ id: "3ohzdIrmkBViO8uouc" },
+
+		]
 		return (
 			<div>
 				<div className="left-scene">
 					<SearchBar />
 					<div className="selected-gif">
-						<Gif />
-					<div>
+						<Gif id="xT9IgDEI1iZyb2wqo8" />
+					</div>
 				</div>
-				<div className="right-scene"></div>
+				<div className="right-scene">
+					<GifList gifs={gifs} />
+				</div>
 			</div>
 		);
 	}
